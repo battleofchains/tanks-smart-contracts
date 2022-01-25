14b8d589a08eb8dfab685e314ab1ea3496125ca7
Alexey Mihailov <alexey.mihailov@ubex.com>
2022-01-25 19:49:09 +0300

    opensea support

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -161,4 +161,17 @@ contract MyNFT is ERC721Enumerable, Ownable {
 
         return true;
     }
+
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public override view returns (bool isOperator) {
+      // if OpenSea's ERC721 Proxy Address is detected, auto-return true
+        if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
+            return true;
+        }
+        
+        // otherwise, use the default ERC721.isApprovedForAll()
+        return ERC721.isApprovedForAll(_owner, _operator);
+    }
 }
